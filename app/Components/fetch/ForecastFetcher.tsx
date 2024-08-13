5f9ab48982f6760b030060c4f758093d21c11eb9
roman <roman.pavlenko.job@gmail.com>
2024-08-13 12:12:19 +0300

    redux setup

---
+++ app/Components/fetch/ForecastFetcher.tsx
@@ -0,0 +1,6 @@
+const key : string = "6fbc02eb3e68489fb0290057241308";
+const mainAdress : string = "http://api.weatherapi.com/v1/"; 
+
+export default async function ForecastFetcher(params:any) {
+  
+}
